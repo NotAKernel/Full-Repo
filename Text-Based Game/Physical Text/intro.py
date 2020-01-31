a7e4f4302298b48da9aa8b3b17a95962d182dbb9
Daniel Yermak <daniely101202@gmail.com>
2020-01-31 15:00:43 +0000

    Full Repo

---
+++ Text-Based Game/Physical Text/intro.py
@@ -0,0 +1,5 @@
+print("Hello, weary wanderer! Would you like to rest at the established 'Holy HotELL...?")
+q1 = input("Well, would you? (Yes or No): ")
+a1 = "Yes"
+if q1 == a1.lower():
+    print("Ah, splendid! Right this way. (Follow him?): ")
